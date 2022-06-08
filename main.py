ba163a99798b145349a9375cd12b58d470f0e928
Muaaz Rajput <muaazrajput11@gmail.com>
2022-06-08 17:06:37 +0200

    Added comments

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import requests
 
 query = input("Search for a city...\n")
 
+# Generate parameters as a dict
 params = {'key':'f68f78080ffc4dc7863174330202207',
           'q':query}
 
+# Make an API call using the specified parameters
 r = requests.get("http://api.weatherapi.com/v1/current.json", params = params)
 
+# Save the returned data as JSON object in the form of a python dict
 data = r.json()
 
+# Parse the JSON object using keys and save needed data
 city = data['location']['name']
 region = data['location']['region']
 country = data['location']['country']
@@ -22,6 +26,7 @@ wind_angle = data['current']['wind_degree']
 condition = data['current']['condition']['text']
 visibility = data['current']['vis_km']
 
+# Print the data in a formatted manner
 print(f"{city}, {region}, {country}\n{time}")
 print(f"Temperature: {temp}\nFeels like: {feels_like}\nHumidity: {humidity}")
 print(f"Skies: {condition}\nVisibility: {visibility}")
